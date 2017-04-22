e94ad639d745e190a56f4bcf71cef0607a1d4e1e
Jens Nevens <jnevens@nic63.usr.hydra.brussel.vsc>
2017-04-22 15:58:41 +0200

    Added job scripts to this branch

---
+++ hydra_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#PBS -l nodes=1:ppn=4
+#PBS -l mem=16gb
+#PBS -l walltime=72:00:00
+#PBS -o dqn.out
+#PBS -e dqn.err
+#PBS -N dqn
+#PBS -V
+
+module add openblas
+module add CUDA/8.0.61
+module add cuDNN/4.0
+
+cd $HOME
+source .bashrc
+source activate dqn
+cd DQN-agent
+python main.py --use_gpu 0
+
