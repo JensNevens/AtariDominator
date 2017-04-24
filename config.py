440c8301c33b7d98baadfc794d57fe63a1a57439
Jens Nevens <jnevens@vub.ac.be>
2017-04-24 15:19:34 +0200

    Smaller memory size. Faster learning. Hell yeah

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class AgentConfig(object):
   display = False
 
   max_step = 5000 * scale
-  memory_size = 100 * scale
+  memory_size = 25 * scale
 
   batch_size = 32
   random_start = 30
@@ -15,7 +15,7 @@ class AgentConfig(object):
   learning_rate_decay = 0.96
   learning_rate_decay_step = 5 * scale
 
-  ep_end = 0.01
+  ep_end = 0.1
   ep_start = 1.
   ep_end_t = memory_size
 
